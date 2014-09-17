e2f6e4b18e695ea3793205d63dad1666492b0953
benedictpennyinskip <44841@longroad.ac.uk>
2014-09-17 14:16:36 +0100

    gf

--- variables/Development exersize 4.py
+++ variables/Development exersize 4.py
@@ -2,9 +2,9 @@
 #15-09-2014
 #Development exercises  4
 
-length = int(input("what is the length of the pool?"))
-depth = int(input("what is the depth of the pool?"))
-width = int(input("what is the width of the pool?"))
+length = float(input("what is the length of the pool?"))
+depth = float(input("what is the depth of the pool?"))
+width = float(input("what is the width of the pool?"))
 
 answer = length * depth * width
 print("the amount of water needed is {0}".format(answer))
