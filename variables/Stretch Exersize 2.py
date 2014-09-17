98c44b45c67e6373a1baac35eac2885b4790c15f
benedictpennyinskip <44841@longroad.ac.uk>
2014-09-17 14:17:12 +0100

    #Ben Penny-Inskip #15-09-2014 #Stretch Exersise 2.2

---
+++ variables/Stretch Exersize 2.py
@@ -0,0 +1,15 @@
+#Ben Penny-Inskip
+#15-09-2014
+#Stretch Exersise 2.2
+
+import math
+
+radius = float(input("what is the radius of your circle?"))
+
+
+ 
+area = math.pi * radius**2
+area = round(area,2)
+    
+print("The area is:{0}".format(area))
+
